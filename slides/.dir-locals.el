e4ea9d9e53caf681af456c310c8f8280f7a38a61
Valentin Robert <valentin.robert.42@gmail.com>
2018-11-14 02:45:13 -0800

    progress

---
+++ slides/.dir-locals.el
@@ -0,0 +1,6 @@
+((latex-mode
+  (TeX-command-extra-options . "-shell-escape")
+  (TeX-engine . xetex)
+  ;; (TeX-master . "dissertation")
+  (pdf-latex-command . "xelatex")
+  (shell-escape-mode . "-shell-escape")))
